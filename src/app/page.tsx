77f7bf63dc55f8ef4cec75e4bf2dc4d0aa2faea4
Ahmad <ranaahmadranaahmad741@gmail.com>
2026-09-06 01:15:58 +0500

    vercel updation
    vercel updation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,12 @@
 
 import Navbar from '../../components/Navbar';
 import Hero from '../../components/Hero';
-import ContentRow from '../../components/ContentRow';
-
-
 
 export default function NetflixHome() {
   return (
     <div className="bg-[#141414] text-white min-h-screen overflow-x-hidden">
       <Navbar />
       <Hero />
-
     </div>
   );
 }
